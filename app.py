ebe20cea1cdf5d2e7a28a48a6e02dc0ba8d05c9a
Gaurav Arora <gauravarora@Gauravs-MacBook-Pro.local>
2023-09-29 15:19:49 -0700

    working model

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import export
 from flask import Flask, request, jsonify, render_template
 import requests
 import os
@@ -7,31 +6,34 @@ import json
 # Create a Flask application
 app = Flask(__name__)
 
-# Load API key and model ID from config.json
-with open('config.json') as config_file:
-    config = json.load(config_file)
-    anyscale_api_key = config["anyscale_api_key"]
-    model_id = config["model_id"]
 
 # Define the AnyScale API base URL
-#export OPENAI_API_BASE='https://api.endpoints.anyscale.com/v1'
 base_url = 'https://api.endpoints.anyscale.com/v1/'
 
 # Function to send user input to the chatbot and get a response
 def send_user_input(user_input):
     try:
-        # Construct the URL for your specific model
-        url = f'{base_url}{model_id}/completions'
+        # Fetch the environment variables
+        api_base = os.getenv("OPENAI_API_BASE")
+        token = os.getenv("OPENAI_API_KEY")
+
+        # Construct the URL
+        url = f"{api_base}/chat/completions"
 
         # Set up headers with the Authorization header
         headers = {
-            'Authorization': f'Bearer {anyscale_api_key}',
+            'Authorization': f"Bearer {token}",
             'Content-Type': 'application/json',
         }
 
-        # Prepare the request body with user input
+        # Prepare the request body with system message, user input, and model details
         body = {
-            "messages": [{"role": "user", "content": user_input}],
+            "model": "meta-llama/Llama-2-70b-chat-hf",
+            "messages": [
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": user_input}
+            ],
+            "temperature": 0.7
         }
 
         # Make a POST request to the AnyScale API
@@ -42,16 +44,13 @@ def send_user_input(user_input):
             chatbot_response = data.get('choices')[0].get('message').get('content')
             return chatbot_response
         else:
-            return 'Sorry, I couldn\'t fetch a response at the moment.'
+            return f"Error {response.status_code}: {response.text}"
     except Exception as e:
+       # print(f"An error occurred: {str(e)}")  # Logging the error
         return f"An error occurred: {str(e)}"
 
-# Add a route to serve the chat.html file
-@app.route('/chatpage', methods=['GET', 'POST'])
-def chat_page():
-    return render_template('chat.html')  # Render the chat.html template
 
-# Modify the /chat route to handle both GET and POST requests
+# Add a route to serve the chat.html file and handle POST requests
 @app.route('/chat', methods=['GET', 'POST'])
 def chat():
     if request.method == 'POST':
@@ -59,9 +58,8 @@ def chat():
         chatbot_response = send_user_input(user_input)
         return jsonify({'chatbot_response': chatbot_response})
     elif request.method == 'GET':
-        # Handle GET requests here
-        initial_message = "Welcome to the chat! Start by typing a message."
-        return jsonify({'chatbot_response': initial_message})
+        return render_template('chat.html')  # Render the chat.html template
+
 
 if __name__ == '__main__':
     app.run(debug=True)
